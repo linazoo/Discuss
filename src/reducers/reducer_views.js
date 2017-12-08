a6419136756131ce5bfb6388a7b05b090a5efab6
Lina Zoo <linazooper@gmail.com>
2017-12-07 22:31:43 -0600

    added reducer to handle response and add views to state

--- src/reducers/reducer_views.js
+++ src/reducers/reducer_views.js
@@ -1,4 +1,9 @@
+import { FETCH_VIEWS } from '../actions/index';
 export default function(state = null, action) {
+    switch (action.type) {
+    case FETCH_VIEWS:
+        return action.payload.data.data.children;    
+    }
     console.log('action received', action);
     return state;
     
