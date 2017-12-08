036c97173babbd0994cb2b4434b18b44a4b8898c
Lina Zoo <linazooper@gmail.com>
2017-12-07 22:05:13 -0600

    added axios - forming url for request - passing request to payload in action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,19 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const ROOT_URL = 'https://www.reddit.com/r/changemyview/search.json?q=';
-// const END_URL = "&restrict_sr=on&sort=relevance&t=all";
+const ROOT_URL = 'https://www.reddit.com/r/changemyview/search.json?q=';
+const END_URL = "&restrict_sr=on&sort=relevance&t=all";
 
-// export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const FETCH_VIEWS = 'FETCH_VIEWS';
 
-// export function fetchWeather(term) {
-//     const cleanTerm = term.split(" ").join("+");
-//     const url = `${ROOT_URL}${cleanTerm}${END_URL}`;
-//     const request = axios.get(url);
+export function fetchViews(term) {
+    const cleanTerm = term.split(" ").join("+");
+    const url = `${ROOT_URL}${cleanTerm}${END_URL}`;
+    const request = axios.get(url);
 
 //     console.log('Request:', request);
 
-//     return {
-//         type: FETCH_WEATHER,
-//         payload: request
-//     };
-// }
+    return {
+        type: FETCH_VIEWS,
+        payload: request
+    };
+}
