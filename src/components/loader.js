3295ac655f54fc5182049a11a146d82f037b2598
Lina Zoo <linazooper@gmail.com>
2017-12-10 13:35:10 -0600

    added a loader component and imported into App.js

---
+++ src/components/loader.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+export default class Loader extends Component {
+
+    render() {
+        return (
+            <h2>THIS IS A LOADER BICH</h2>
+        );
+    }
+}
