5512886104043bbc14e118ef72e31b5d67501939
John Lyden <johnlydennc@gmail.com>
2017-12-16 19:03:53 -0600

    fixed indentation

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,23 +7,23 @@ export const FETCH_VIEWS = 'FETCH_VIEWS';
 export const FETCH_VIEW = 'FETCH_VIEW';
 
 export function fetchViews(term) {
-    const cleanTerm = term.split(" ").join("+");
-    const url = `${ROOT_URL}${cleanTerm}${END_URL}`;
-    const request = axios.get(url);
+	const cleanTerm = term.split(" ").join("+");
+	const url = `${ROOT_URL}${cleanTerm}${END_URL}`;
+	const request = axios.get(url);
 
-    console.log('Request:', request);
+	console.log('Request:', request);
 
-    return {
-        type: FETCH_VIEWS,
-        payload: request
-    };
+	return {
+		type: FETCH_VIEWS,
+		payload: request
+	};
 }
 
 export function fetchView(url) {
-    const request = axios.get(url);
+	const request = axios.get(url);
 
-    return {
-        type: FETCH_VIEW,
-        payload: request
-    };
+	return {
+		type: FETCH_VIEW,
+		payload: request
+	};
 }
--- src/reducers/reducer_loading.js
+++ src/reducers/reducer_loading.js
@@ -1,13 +1,13 @@
 import { FETCH_VIEW } from '../actions/index';
-export default function(state = {}, action) {
-    switch (action.type) {
-    case FETCH_VIEW:
-        return Object.assign({}, state, {
-            view: action.payload.data[0].data.children[0].data,
-            replies: action.payload.data[1].data.children
-        })
-    }
-    console.log('action received', action);
-    return state;
-    
+export default function (state = {}, action) {
+	switch (action.type) {
+		case FETCH_VIEW:
+			return Object.assign({}, state, {
+				view: action.payload.data[0].data.children[0].data,
+				replies: action.payload.data[1].data.children
+			})
+	}
+	console.log('action received', action);
+	return state;
+
 }
