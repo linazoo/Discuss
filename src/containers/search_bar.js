815365296bbf3302c5523aa9d0ada094835a3d3f
John Lyden <johnlydennc@gmail.com>
2017-12-16 18:28:11 -0600

    fixed indenation

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,47 +5,47 @@ import { fetchViews } from '../actions/index';
 
 
 class SearchBar extends Component {
-    constructor(props) {
-        super(props)
-        
-        this.state= { term: '' };
+	constructor(props) {
+		super(props)
 
-        this.onInputChange= this.onInputChange.bind(this);
-        this.onFormSubmit= this.onFormSubmit.bind(this);
-    }
+		this.state = { term: '' };
 
-    onInputChange(event) {
-        this.setState({ term: event.target.value });
-    }
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
+	}
 
-    onFormSubmit(event) {
-        event.preventDefault();
-        this.props.fetchViews(this.state.term);
-        this.setState({term: ''});
+	onInputChange(event) {
+		this.setState({ term: event.target.value });
+	}
 
-    }
+	onFormSubmit(event) {
+		event.preventDefault();
+		this.props.fetchViews(this.state.term);
+		this.setState({ term: '' });
 
-    render() {
-        const style = {
-            'margin': '40px'
-          }
-        return (
-            <form onSubmit={this.onFormSubmit} className="input-group" style={style}>
-                <input 
-                    placeholder="Search for a topic you'd change your mind about"
-                    className="form-control"
-                    value={ this.state.term }
-                    onChange={ this.onInputChange } />
-                <span className="input-group-btn">
-                    <button type="submit" className="btn btn-secondary">Search</button>
-                </span>
-            </form>
-        );
-    }
+	}
+
+	render() {
+		const style = {
+			'margin': '40px'
+		}
+		return (
+			<form onSubmit={this.onFormSubmit} className="input-group" style={style}>
+				<input
+					placeholder="Search for a topic you'd change your mind about"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange} />
+				<span className="input-group-btn">
+					<button type="submit" className="btn btn-secondary">Search</button>
+				</span>
+			</form>
+		);
+	}
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ fetchViews }, dispatch);
+	return bindActionCreators({ fetchViews }, dispatch);
 }
 
 export default connect(null, mapDispatchToProps)(SearchBar);
