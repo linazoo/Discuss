69db3001cb79d76d167d0d67e79a18c13843a0cd
lina zoo <linazooper@gmail.com>
2018-01-04 20:25:19 -0600

    split up the comment section into two columns and put a limit on the lenght of a reply

--- src/components/view_show.js
+++ src/components/view_show.js
@@ -6,20 +6,20 @@ export default class ViewShow extends Component {
 		super(props)
 	}
 
-	renderReplies(replyData) {
-		const myStyle = {
-		}
-		return (
-			<div className="bubble-arrow-right" style={myStyle}>{replyData.data.body}</div>
-		);
-	}
+	// renderReplies(replyData) {
+	// 	const myStyle = {
+	// 	}
+	// 	return (
+	// 		<div className="bubble-arrow-right" style={myStyle}>{replyData.data.body}</div>
+	// 	);
+	// }
 
 	renderAllReplies(replies){
 		let num = 1;
 		const yo = replies.map((reply) => {
 			num = num+1;
 			return (
-				<div className={num % 2 === 0 ? "bubble-arrow-right" : "bubble-arrow-left"}>{reply.data.body}</div>
+				<div className={num % 2 === 0 ? "bubble-arrow-right" : "bubble-arrow-left"}>{reply.data.body.substring(0,300)}</div>
 			)
 		});
 		return yo	
@@ -30,9 +30,11 @@ export default class ViewShow extends Component {
 		const replies = this.props.replies;
 		return (
 			<div className="">
-			<h4>{ title }</h4>
-				{ this.renderAllReplies(replies) }
+				<h4>{ title }</h4>
+				<div className="replies-container">
+					{ this.renderAllReplies(replies) }
 				</div>
+			</div>
 		);
 	}
 }
