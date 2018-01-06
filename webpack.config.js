ddb9e680bbf560882113838ccb918bf26f8e62a2
johnlyden <john.lyden86@gmail.com>
2018-01-06 12:40:05 -0600

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,20 +9,18 @@ module.exports = {
   },
   module: {
     loaders: [{
-      test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel-loader',
+      loader: 'babel',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
-    },
-    {
-      test: /\.(png|jpg)$/,
-      loader: 'url?limit=25000'
-    }
-  ]
+    }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
   }
 };
