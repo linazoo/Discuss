bc807bc6df52577b7fc59451acda38d78225f4ba
johnlyden <johnlydennc@gmail.com>
2018-01-20 21:44:15 -0600

    made fetchviewsbycategory action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const ROOT_URL = 'https://www.reddit.com/r/changemyview/search.json?q=';
+// new root url for the new/hot/etc 
+const CATEGORY_URL = 'https://www.reddit.com/r/changemyview/';
+// becuase its a difernt url than the one above
 const END_URL = "&restrict_sr=on&sort=relevance&t=all";
 
 export const FETCH_VIEWS = 'FETCH_VIEWS';
@@ -14,12 +17,21 @@ export function emptyViews() {
   }
 }
 
+// make action creator for fetchViewsByCateogry
+export function fetchViewsByCategory(category) {
+	const url =`${CATEGORY_URL}${category}/.json`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_VIEWS,
+		payload: request
+	};
+}
+
 export function fetchViews(term) {
 	const cleanTerm = term.split(" ").join("+");
 	const url = `${ROOT_URL}${cleanTerm}${END_URL}`;
 	const request = axios.get(url);
-
-	console.log('Request:', request);
 
 	return {
 		type: FETCH_VIEWS,
