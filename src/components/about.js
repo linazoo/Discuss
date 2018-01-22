be73373f5ddd78632e3f5032a7fdb02bf366aa39
johnlyden <johnlydennc@gmail.com>
2018-01-21 21:41:52 -0600

    about page now includes more information

--- src/components/about.js
+++ src/components/about.js
@@ -4,16 +4,26 @@ const About = (props) => {
   return (
     <div>
       <div className="main-quote">
-        <h2>"In order to resolve our differences we must first understand them"</h2>
+        <h1>A 'Change My View' Subreddit Reader</h1>
+        <h2>What is /r/changemyview?</h2>
       </div>
       <div className="cmw-explained">
-        <p>What is /r/changemyview?
-  CMV is a subreddit dedicated to the civil discourse of opinions, and is built around the idea that in order to resolve our differences, we must first understand them. We believe that productive conversation requires respect and openness, and that certitude is the enemy of understanding.
-
-  That's why CMV is the perfect place to post an opinion you're open to changing. We're not looking to host aggressive debates, or encourage judgement, but help each other understand different perspectives.
-
-  changemyview.net
-        </p>
+        <br />
+        <p>CMV is a subreddit dedicated to the civil discourse of opinions, and is built around the idea that in order to resolve our differences, we must first understand them. We believe that productive conversation requires respect and openness, and that certitude is the enemy of understanding.
+          That's why CMV is the perfect place to post an opinion you're open to changing. We're not looking to host aggressive debates, or encourage judgement, but help each other understand different perspectives.
+          <br /> 
+        <h3> What is a View?</h3>
+        What is a 'view'?
+        One definition of 'view' (noun) is "a particular way of considering or regarding something; an attitude or opinion," and one definition of 'view' (verb) is "to look at or inspect."
+        Both definitions are helpful in explaining what is posted here, as it can be useful to consider the literal meaning. We aren't necessarily defined by our views, but what our views lead us to do, and so a degree of detachment can be useful.
+        In the physical world, an object can look different depending on where you stand. Similarly, your standpoint on an issue can depend on your unique background and experiences.
+          changemyview.net
+          <h3> What is a 'change'?</h3>
+          The definition of 'change' (verb) is "make or become different."
+          Following on from the previous segment, we therefore believe that a change in view simply means a new perspective. Perhaps, in the example of literally looking at something, you've taken a step to the side; or a few steps; or you've moved around and now stand behind it. Maybe you haven't 'moved', but it looks slightly different to you now; in a new light.
+          A change is not necessarily a reversal or '180' of opinion.
+          </p>
+          
       </div>
     </div>
   );
